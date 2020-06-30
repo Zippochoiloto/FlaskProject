6018ac68776a7ed449d51133307bcab04f2c120b
TheAnh <anhluong@tamdongtam.vn>
2020-06-30 18:36:38 +0700

    add

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  return "Hello World"
+def index():
+  return render_template('index.html')
+
+@app.route('/home/users/<string:name>/posts/<int:id>',methods=['GET'])
+def hello(name, id):
+  return "Hello, " + name + ',your id is :' + str(id)
+
+def get_req():
+  return 'You can only get this webpage'
 
 if __name__ == "__main__":
   app.run(debug=True)
