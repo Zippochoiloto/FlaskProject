a80feaaa2a9ef0feb6d0a3d69060d5d5b1c9a27d
TheAnh <anhluong@tamdongtam.vn>
2020-07-01 16:49:20 +0700

    fixUI

--- app.py
+++ app.py
@@ -2,9 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+all_posts = [
+  {
+    'title': 'Post 1',
+    "content": "this is the content of post1",
+    'author': 'The Anh'
+  },
+  {
+    'title': 'Post 2',
+    "content": "this is the content of post2"
+  },
+  {
+    'title': 'Post 3',
+    "content": "this is the content of post3"
+  },
+]
+
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+  return render_template('post.html', posts=all_posts)
 
 @app.route('/home/users/<string:name>/posts/<int:id>',methods=['GET'])
 def hello(name, id):
